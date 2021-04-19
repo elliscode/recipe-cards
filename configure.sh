f73471dbff7e4bf55bb34c932e45fc3f5aea19c3
unknown <programmer08054@gmail.com>
2021-04-19 18:56:58 -0400

    working configure script

---
+++ configure.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/elliscode/serving-calculator.git serving-calculator
+git clone https://github.com/elliscode/container-weights-calculator.git container-weights-calculator
