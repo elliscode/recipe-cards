ca43ffe06876fef42242313daa7877a6eae4c6a3
unknown <programmer08054@gmail.com>
2022-03-19 18:22:52 -0400

    removed unused parts

--- configure.sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/elliscode/serving-calculator.git serving-calculator
-git clone https://github.com/elliscode/container-weights-calculator.git container-weights-calculator
