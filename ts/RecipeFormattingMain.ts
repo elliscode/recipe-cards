828a2c980a5b91d8ca144819bee7ddb4c8073852
unknown <programmer08054@gmail.com>
2022-03-24 08:48:15 -0400

    no more jacascript flash

--- ts/RecipeFormattingMain.ts
+++ ts/RecipeFormattingMain.ts
@@ -1,5 +1,11 @@
 import RecipeFormatting from './RecipeFormatting'
 
+const noScript = document.getElementById('no-script');
+const scriptPlacement = document.getElementById('script-placement');
+if(noScript && scriptPlacement) {    
+    scriptPlacement.innerHTML = noScript.textContent!;
+}
+
 const r: RecipeFormatting = new RecipeFormatting();
 r.parseRecipes();
 r.buildSections();
